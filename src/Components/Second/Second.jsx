3279f861ca4ba8bdf2477451dd2bad4e5d50eef5
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-07-19 17:55:01 +0500

    Fashion

--- src/Components/Second/Second.jsx
+++ src/Components/Second/Second.jsx
@@ -1,22 +1,7 @@
 import React from "react";
 
 const Second = () => {
-  return (
-    <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae cumque,
-        consectetur totam nisi accusamus nostrum eligendi inventore aspernatur
-        impedit doloremque, delectus alias dolorum repudiandae labore
-        reprehenderit magnam libero, fuga molestias!
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas
-        voluptatum expedita provident vitae qui dolorem eveniet ducimus aliquam,
-        maiores autem numquam rem labore tempora doloribus odit molestias nulla
-        iusto. Quos.
-      </p>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Second;
