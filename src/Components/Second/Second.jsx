7ee537a80e119f6d52973ab6182f4e67c2bbcfb9
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-07-20 16:10:26 +0500

    Fashion

--- src/Components/Second/Second.jsx
+++ src/Components/Second/Second.jsx
@@ -5,6 +5,11 @@ const Second = () => {
     <div>
       <p>Second Page</p>
       <h1>Ok</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, mollitia,
+        nisi atque veniam magnam, officiis esse omnis non quo natus adipisci
+        corrupti molestiae sunt. Assumenda harum dicta quaerat dolorum rem?
+      </p>
     </div>
   );
 };
