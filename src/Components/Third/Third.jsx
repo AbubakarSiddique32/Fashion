1fb3afa47df0ff0d6cbeeee1ab555e533880433a
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-07-27 16:20:29 +0500

    Fashion

--- src/Components/Third/Third.jsx
+++ src/Components/Third/Third.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Third = () => {
-  return <div></div>;
+  return (
+    <div>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo inventore
+        ex nam omnis impedit ratione adipisci quam, vitae excepturi aperiam
+        ipsam necessitatibus dolore saepe earum dolorem rem. Magnam, ad odio!
+      </p>
+    </div>
+  );
 };
 
 export default Third;
