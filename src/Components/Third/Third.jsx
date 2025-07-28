c41e04a10c8ec75959bdc20061ed77f723fc5722
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-07-28 17:43:51 +0500

    Fashion

--- src/Components/Third/Third.jsx
+++ src/Components/Third/Third.jsx
@@ -4,6 +4,12 @@ const Third = () => {
   return (
     <div>
       <h1>ok</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+        voluptatem magni placeat aliquam. A iure consectetur amet molestias
+        inventore asperiores itaque modi fuga ullam maiores, exercitationem
+        recusandae iste deserunt. Maxime.
+      </p>
     </div>
   );
 };
