5c034e514e16f2d673cee4c276c69edd4e6fcc8f
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-07-30 12:53:22 +0500

    Fashion

--- src/Components/Third/Third.jsx
+++ src/Components/Third/Third.jsx
@@ -3,12 +3,8 @@ import React from "react";
 const Third = () => {
   return (
     <div>
-      <h1>ok</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-        voluptatem magni placeat aliquam. A iure consectetur amet molestias
-        inventore asperiores itaque modi fuga ullam maiores, exercitationem
-        recusandae iste deserunt. Maxime.
+        third <section></section>
       </p>
     </div>
   );
