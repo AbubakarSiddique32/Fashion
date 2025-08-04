f658f353c712a52ef664730a2f3c6c226e6979ca
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-08-04 21:25:11 +0500

    Fashion

--- src/Components/Third/Third.jsx
+++ src/Components/Third/Third.jsx
@@ -12,6 +12,12 @@ const Third = () => {
         Aspernatur veniam obcaecati explicabo tempore aliquid ea incidunt
         eligendi similique nulla asperiores.
       </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        maiores dolorem delectus harum et unde at quaerat? Eaque, tempore sed!
+        Esse molestiae nobis tenetur vel dolorem repudiandae nihil unde
+        consectetur.
+      </p>
     </div>
   );
 };
