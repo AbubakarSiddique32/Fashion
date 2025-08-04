d986a1ac169ae84cf72799061c3500cfcf89dd95
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-08-04 21:24:49 +0500

    Fashion

--- src/Components/Third/Third.jsx
+++ src/Components/Third/Third.jsx
@@ -5,6 +5,13 @@ const Third = () => {
     <div className=" bg-blue-200">
       <p>third hi this is third section</p>
       <h6>ok heading 6</h6>
+      <h4>hello world</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti velit
+        porro laboriosam alias necessitatibus esse quo voluptatem animi?
+        Aspernatur veniam obcaecati explicabo tempore aliquid ea incidunt
+        eligendi similique nulla asperiores.
+      </p>
     </div>
   );
 };
