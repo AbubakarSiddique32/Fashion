5bcb14b4e83187bb9093cccea6c3ef83de111cb8
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2025-09-10 16:35:32 +0500

    Fashion

--- src/Components/Third/Third.jsx
+++ src/Components/Third/Third.jsx
@@ -3,44 +3,7 @@ import React from "react";
 const Third = () => {
   return (
     <div className=" bg-blue-200">
-      <p>third hi this is third section</p>
-      <h6>ok heading 6</h6>
-      <h4>hello world</h4>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti velit
-        porro laboriosam alias necessitatibus esse quo voluptatem animi?
-        Aspernatur veniam obcaecati explicabo tempore aliquid ea incidunt
-        eligendi similique nulla asperiores.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-        maiores dolorem delectus harum et unde at quaerat? Eaque, tempore sed!
-        Esse molestiae nobis tenetur vel dolorem repudiandae nihil unde
-        consectetur.
-      </p>
-        <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-        maiores dolorem delectus harum et unde at quaerat? Eaque, tempore sed!
-        Esse molestiae nobis tenetur vel dolorem repudiandae nihil unde
-        consectetur.
-      </p>
-        <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-        maiores dolorem delectus harum et unde at quaerat? Eaque, tempore sed!
-        Esse molestiae nobis tenetur vel dolorem repudiandae nihil unde
-        consectetur.
-      </p>
-        <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-        maiores dolorem delectus harum et unde at quaerat? Eaque, tempore sed!
-        Esse molestiae nobis tenetur vel dolorem repudiandae nihil unde
-        consectetur.
-      </p>
-
-       
     
-      
-      <p>Lorem</p>
     </div>
   );
 };
