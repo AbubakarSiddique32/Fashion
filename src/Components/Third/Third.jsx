62b6033093d30b1a3beb6266d0792f027a75122a
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2025-09-12 17:33:42 +0500

    Fashion

--- src/Components/Third/Third.jsx
+++ src/Components/Third/Third.jsx
@@ -6,6 +6,7 @@ const Third = () => {
     <p>
     lorem
     ok now im doing work as a bidder in iconic studio
+    <h1 className=" bg-black text-red text-[50px] w-full">Header</h1>
     </p>
     </div>
   );
