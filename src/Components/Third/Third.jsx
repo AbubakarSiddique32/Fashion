bf654e2c7f6b338dad9a45b9ebb15f1d155365cf
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2025-09-14 16:20:16 +0500

    Fashion

--- src/Components/Third/Third.jsx
+++ src/Components/Third/Third.jsx
@@ -3,14 +3,7 @@ import React from "react";
 const Third = () => {
   return (
     <div className=" bg-blue-200">
-    <p>
-    lorem
-    ok now im doing work as a bidder in iconic studio
-    <h1 className=" bg-black text-red text-[50px] w-full">Header</h1>
-    </p>
-    <p>hello my friend how are you you are fine hehehe</p>
-    <p>ok my friend</p>
-    <br></br>
+   <h1>Header</h1>
     </div>
   );
 };
