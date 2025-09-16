912b2809157d062b6c488ae75402123262c2da63
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2025-09-16 16:12:19 +0500

    Fashion

--- src/Components/Third/Third.jsx
+++ src/Components/Third/Third.jsx
@@ -4,10 +4,7 @@ const Third = () => {
   return (
     <div className=" bg-blue-200">
    <h1>Header</h1>
-   <p>ok i know what's going on</p>
-   <h2>heading 2</h2>
-   <h3>heading 3</h3>
-   <h4>heading 4</h4>
+   <p>ok i get that</p>
    <button>Click Me</button>
     </div>
   );
