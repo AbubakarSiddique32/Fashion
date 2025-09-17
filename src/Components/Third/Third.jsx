8d9c1b1875c773da26735607da7ecd68ad12d25f
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2025-09-17 14:37:45 +0500

    Fashion

--- src/Components/Third/Third.jsx
+++ src/Components/Third/Third.jsx
@@ -5,7 +5,10 @@ const Third = () => {
     <div className=" bg-blue-200">
    <h1>Header</h1>
    <p>ok i get that</p>
+   <p>Yollo Lopo</p>
+   <div>
    <button>Click Me</button>
+   </div>
     </div>
   );
 };
