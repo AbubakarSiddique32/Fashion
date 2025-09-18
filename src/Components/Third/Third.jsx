642ec3b50fcd23aa509fa983c8d5d29f1d2fecc0
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2025-09-18 17:37:03 +0500

    Fashion

--- src/Components/Third/Third.jsx
+++ src/Components/Third/Third.jsx
@@ -3,10 +3,9 @@ import React from "react";
 const Third = () => {
   return (
     <div className=" bg-blue-200">
-   <h1>Header</h1>
-   <p>ok i get that</p>
-   <p>Yollo Lopo</p>
-   <h4>Git-Hub</h4>
+   <img></img>
+   <h1>OK</h1>
+   
    <div>
    <button>Click Me</button>
    </div>
