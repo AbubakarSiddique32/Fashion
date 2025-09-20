18a6a5bdb2c80c9183e8a52cbc828570d8830667
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2025-09-20 15:36:04 +0500

    Fashion

--- src/Components/Third/Third.jsx
+++ src/Components/Third/Third.jsx
@@ -5,7 +5,7 @@ const Third = () => {
     <div className=" bg-blue-200">
    <img></img>
    <h1>OK</h1>
-   <p>lorem 34 is the raw text</p>
+   <p>lorem 34 is the raw text yes you get that what im trying to say you</p>
    
    <div>
    <button>Click Me</button>
