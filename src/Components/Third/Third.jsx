f1de641beb85cca6ff77c49d5028b22d12db0a0b
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2025-09-20 15:36:45 +0500

    Fashion

--- src/Components/Third/Third.jsx
+++ src/Components/Third/Third.jsx
@@ -6,6 +6,7 @@ const Third = () => {
    <img></img>
    <h1>OK</h1>
    <p>lorem 34 is the raw text yes you get that what im trying to say you</p>
+   <h2>wow good news for you now lets celeberate that</h2>
    
    <div>
    <button>Click Me</button>
