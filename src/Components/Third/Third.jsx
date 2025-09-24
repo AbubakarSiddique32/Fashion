2d69282bfd8821e17e5e9f9ccfe359e0823ad7be
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2025-09-24 16:13:22 +0500

    Fashion

--- src/Components/Third/Third.jsx
+++ src/Components/Third/Third.jsx
@@ -3,21 +3,9 @@ import React from "react";
 const Third = () => {
   return (
     <div className=" bg-blue-200">
-   <img></img>
-   <h1>OK</h1>
-   <p>lorem 34 is the raw text yes you get that what im trying to say you</p>
-   <h2>wow good news for you now lets celeberate that</h2>
-   <div>
-    <p>ok</p>
-   </div>
-   <div>
-    <h1>ok</h1>
-    <p>ok i am workin for you</p>
-   </div>
-   <div><h1>ok</h1></div>
-   <div>
+    <div>
    <button>Click Me</button>
-   </div>
+    </div>
     </div>
   );
 };
