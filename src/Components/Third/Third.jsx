0d5409872fdb32ea28105e3eaf29369ffe67c2f2
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2025-10-19 01:09:05 +0500

    Fashion

--- src/Components/Third/Third.jsx
+++ src/Components/Third/Third.jsx
@@ -11,13 +11,7 @@ const Third = () => {
       <button className="bg-black rounded"> Click </button>
       <h1 className="text-[16px] p-[5px]">heading one</h1>
       <ul className=" flex items-center gap-3">
-        <li>Home</li>
-        <li>Main</li>
-        <li>Shop</li>
-        <li>Shop All</li>
-        <li>Collections</li>
-        <li>All Collections</li>
-        <li><a></a></li>
+      <li>Website</li>
       </ul>
     </div>
     </div>
