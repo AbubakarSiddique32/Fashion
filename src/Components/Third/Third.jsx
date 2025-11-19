974dcf7dcd803a5ffa6b66a8fec23e2892184405
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2025-11-19 15:36:11 +0500

    Fashion

--- src/Components/Third/Third.jsx
+++ src/Components/Third/Third.jsx
@@ -14,6 +14,7 @@ const Third = () => {
       <li className=" text-[20px]">Blogs</li>
       <li className=" bg-red">List</li>
       <li>Done</li>
+      <li>Collections</li>
       <li>Listing</li>
 
       </ul>
