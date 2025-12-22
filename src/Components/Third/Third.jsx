098b139dcb3cabf6458b8281cb96c2c85444d884
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2025-12-22 11:58:51 +0500

    Fashion

--- src/Components/Third/Third.jsx
+++ src/Components/Third/Third.jsx
@@ -13,6 +13,7 @@ const Third = () => {
       <ul className=" flex items-center gap-3">
      <li className=" bg-black text-[16px]">Shop</li>
      <li className=" bg-black text-[16px]">List</li>
+     <li>Third</li>
      
 
     
