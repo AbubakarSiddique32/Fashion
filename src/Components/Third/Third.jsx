cc3279710e50eaba32106d3b5f447699de4ca99c
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2026-01-22 00:31:21 +0500

    Fashion

--- src/Components/Third/Third.jsx
+++ src/Components/Third/Third.jsx
@@ -7,6 +7,7 @@ const Third = () => {
     <h1 >Practicing</h1>
     <p>TRhis is my practicing section and im using this section for github activity</p>
     <h4>heading 4</h4>
+    <h2>ok</h2>
  
     </div>
   );
