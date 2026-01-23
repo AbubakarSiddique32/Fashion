04209f0139efc88063a2a79367f0199875dc5667
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2026-01-23 15:31:01 +0500

    Fashion

--- src/Components/Third/Third.jsx
+++ src/Components/Third/Third.jsx
@@ -4,7 +4,7 @@ const Third = () => {
   return (
     <div className=" bg-gray">
 
-    <h1 >Practicing</h1>
+    <h1 className=" bg-black ">Practicing</h1>
     <p>TRhis is my practicing section and im using this section for github activity</p>
     <h4>heading 4</h4>
     <h2>ok</h2>
