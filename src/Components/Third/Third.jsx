bdeb5788e27224f33f5eef5daf4c6b0bdec4c2b4
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2026-01-28 16:44:45 +0500

    Fashion

--- src/Components/Third/Third.jsx
+++ src/Components/Third/Third.jsx
@@ -11,7 +11,7 @@ const Third = () => {
     <h1 className=" bg-black">list</h1>
     <p className=" bg-blac text-[20px]">Practice</p>
     <h4 className=" bg-white">heading</h4>
-    <p className=" bg-black">ok</p>
+    <p className=" bg-black">ok have fun</p>
  
     </div>
   );
