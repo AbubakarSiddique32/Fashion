63f5aa5712e2c08c8a6529877577b7dd4bbe2863
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2026-01-31 11:53:35 +0500

    Fashion

--- src/Components/Third/Third.jsx
+++ src/Components/Third/Third.jsx
@@ -7,7 +7,7 @@ const Third = () => {
     <h1 className=" bg-black ">Practicing</h1>
     <p className="bg-red">TRhis is my practicing section and im using this section for github activity</p>
     <p>Paragraph</p>
-    <h1>heading</h1>
+    <h1 className=" bg-white">heading</h1>
  
     </div>
   );
