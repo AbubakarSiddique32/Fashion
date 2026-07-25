de5ffdaa135442526636c830e58f2e21a6ed467d
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2026-07-25 15:21:47 +0500

    Anime

--- src/Components/Fourth/Fourth.jsx
+++ src/Components/Fourth/Fourth.jsx
@@ -6,6 +6,7 @@ const Fourth = () => {
     <h1>Secton Fourth</h1>
     <p>lorem</p>
     <p>Fourth</p>
+    <p>FLUX blends easily into water without taste or texture issues, helping you add clean daily fiber to your routine at home, work, or while traveling with simple comfort every day.</p>
     </div>
   )
 }
